f90a61431f2f383b73376c520618b47282f9c4a1
eladlavi29 <47532871+eladlavi29@users.noreply.github.com>
2023-04-22 16:10:26 +0000

    Add files via upload

--- ex4.asm
+++ ex4.asm
@@ -2,4 +2,23 @@
 
 .section .text
 _start:
-#your code here
+	#your code here
+
+	movq head(%rip), %rdi
+	movl $0, %eax
+	testq %rdi, %rdi
+	je end_HW1
+	movl (%rdi), %eax
+
+loop_HW1:
+	#Check if the current node has the same val as Value
+	#movl (%rdi), %edx
+	inc (%rdi)
+	
+	#Iterate through the list
+	movq 4(%rdi), %rdi
+	testq %rdi, %rdi
+	jne .loop_HW1
+
+end_HW1:
+
