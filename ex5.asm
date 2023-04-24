4d858a65bd04e61c715302f6efae00a57ba76815
eladlavi29 <47532871+eladlavi29@users.noreply.github.com>
2023-04-24 14:38:52 +0000

    Add files via upload

--- ex5.asm
+++ ex5.asm
@@ -2,4 +2,36 @@
 
 .section .text
 _start:
-#your code here
+	#your code here
+	#rdi contains the iterator
+	movq root(%rip), %rdi	
+	testq %rdi, %rdi
+	je end_HW1
+	
+	#rdx contains the new node's val
+	movq new_node(%rip), %rdx
+	movq (%rdx), %rdx
+	
+	#find the father of the new leaf new node
+	loop1_HW1:
+		#Check if the current node has the same val as val
+		cmp %rdx, (%rdi)
+		je end_HW1
+		
+		#Iterate through the tree
+		cmp %rdx, (%rdi) #if current node val <= new node val
+		jl newNodeValIsSmaller_HW1
+		
+		#if current node val > new node val
+		mov 16(%rdi), %rdi
+		jmp newNodeValIsSmallerEnd_HW1
+		
+		newNodeValIsSmaller_HW1:
+		mov 8(%rdi), %rdi
+		
+		newNodeValIsSmallerEnd_HW1:
+		
+		testq %rdi, %rdi
+		jne loop1_HW1
+	
+end_HW1:
