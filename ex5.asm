c99c35cd811173d0dc7121036c1e679cd968da37
eladlavi29 <47532871+eladlavi29@users.noreply.github.com>
2023-04-24 20:28:01 +0000

    Add files via upload

--- ex5.asm
+++ ex5.asm
@@ -3,16 +3,19 @@
 .section .text
 _start:
 	#your code here
+	#rsi contains the prev leaf
+	xor %rsi, %rsi
 	#rdi contains the iterator
-	movq root(%rip), %rdi	
+	movq root(%rip), %rdi	 
 	testq %rdi, %rdi
 	je end_HW1
 	
 	#rdx contains the new node's val
 	movq new_node(%rip), %rdx
 	
-	#find the father of the new leaf new node
+	#find the father of the fathr of new node
 	loop1_HW1:
+		mov %rdi, %rsi
 		#Check if the current node has the same val as val
 		cmp %rdx, (%rdi)
 		je end_HW1
@@ -33,7 +36,19 @@ _start:
 		testq %rdi, %rdi
 		jne loop1_HW1
 		
-	#rdi contains the new leaf new node
-	movq $new_node, (%rdi)
+	#set the new node to be a son of the father	
+	cmp %rdx, (%rsi) #if father val <= new node val
+	jl newNodeIsLeftSon_HW1
+	
+	#if current node val > new node val
+	lea 16(%rsi), %rcx
+	jmp newNodeIsLeftSonEnd_HW1
+	
+	newNodeValIsSmaller_HW1:
+	lea 8(%rsi), %rcx
+	
+	newNodeIsLeftSonEnd_HW1:
+	
+	mov %rdx, (%rcx)
 	
 end_HW1:
